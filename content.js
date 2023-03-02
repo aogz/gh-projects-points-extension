e22de1ac5015a73e5599d59aabe3adae8b0604a6
Sasha Ogurtov <sasha@surfly.com>
2023-03-02 14:55:53 +0100

    update selector

--- content.js
+++ content.js
@@ -1,20 +1,20 @@
 document.onkeydown = function (e) {
   if (e.ctrlKey && e.altKey && e.key === "p") {
     console.log("Calculating points...");
-    const tableGroups = document.querySelectorAll('[data-test-id^="table-group-"][role="rowgroup"]');
+    const tableGroups = document.querySelectorAll('[data-testid^="table-group-"][role="rowgroup"]');
 
     for (const tableGroup of tableGroups) {
-      const tableGroupHeader = tableGroup.querySelector('[data-test-id^="table-group-header-"]');
+      const tableGroupHeader = tableGroup.querySelector('[data-testid^="table-group-header-"]');
       const tableGroupRows = tableGroup.querySelectorAll('[role="row"]');
 
       let totalPoints = 0
       let completedPoints = 0
       for (const row of tableGroupRows) {
         const rowIdRegex = /TableRow{id: (.*)}/;
-        const rowIdMatch = row.getAttribute("data-test-id").match(rowIdRegex);
+        const rowIdMatch = row.getAttribute("data-testid").match(rowIdRegex);
         if (rowIdMatch) {
           const rowId = rowIdMatch[1];
-          const pointsCoulmn = `[data-test-id="TableCell{row: ${rowId}, column: Points}"]`
+          const pointsCoulmn = `[data-testid="TableCell{row: ${rowId}, column: Points}"]`
           const isCompleted = row.querySelector('[aria-label="Closed as completed issue"]');
           const rowPoints = parseInt(row.querySelector(pointsCoulmn).innerText) || 0
           totalPoints += rowPoints;
@@ -29,7 +29,7 @@ document.onkeydown = function (e) {
           existingLabel.remove()
         }
 
-        const itemsCounter = tableGroupHeader.querySelector('[data-test-id="column-items-counter"]')
+        const itemsCounter = tableGroupHeader.querySelector('[data-testid="column-items-counter"]')
         const pointsLabel = document.createElement('span');
         pointsLabel.innerText = `Points: ${completedPoints}/${totalPoints}`;
         for (const klass of itemsCounter.classList) {
